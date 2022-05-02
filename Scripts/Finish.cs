2b2d2d04cfd11ffb298a028288a042d65f5a1c00
N0111 <95976517+N0111@users.noreply.github.com>
2022-05-02 19:33:14 +0300

    Add files via upload

---
+++ Scripts/Finish.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unit
+{
+
+    //public class Finish : Gold
+    public class Finish : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var end = Resources.Load<GameObject> ("End");
+            var endInstance = GameObject.Instantiate (end).GetComponent<Unit>();
+            endInstance.SetUnitPosition(new Vector2(438, 256));
+        //    if (money >= 10)
+        //    {
+        //         Debug.Log("Победа! :-) ");
+        //    } else
+        //    {
+        //        Debug.Log("Для победы вам нужно собрать 10 монет");
+        //    }
+        }
+    }
+}
